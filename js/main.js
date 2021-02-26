a336a1f7039816a3c94642d6d2f07cc6d7b8a74f
gonasooc <doupler@gmail.com>
2021-02-26 11:27:04 +0900

    v0.04 - type animation library 적용 완료 - 탭 메뉴 draft 완성

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function(){
+
+    $('a').attr('href', 'javascript:;');
+
     $('.btn_top_wrap').click(function(){
         $('body, html').animate({'scrollTop':0}, 500)
     });
@@ -14,6 +17,48 @@ $(function(){
     $('.btn_all').click(function(){
         $('.works-box ul').show();
     })
+
+    $('.responsive-list > li').click(function(){
+        const IndexNum = $(this).index();
+        console.log(IndexNum);
+        $('.detail-box').fadeIn();
+        $('.cover').fadeIn();
+        $('.responsive-detail-list .detail').eq(IndexNum).fadeIn();
+        $('body, html').css({'overflow':'hidden'});
+    });
+
+    $('.desktop-list > li').click(function(){
+        const IndexNum = $(this).index();
+        console.log(IndexNum);
+        $('.detail-box').fadeIn();
+        $('.cover').fadeIn();
+        $('.desktop-detail-list .detail').eq(IndexNum).fadeIn();
+        $('body, html').css({'overflow':'hidden'});
+    });
+
+    $('.mobile-list > li').click(function(){
+        const IndexNum = $(this).index();
+        console.log(IndexNum);
+        $('.detail-box').fadeIn();
+        $('.cover').fadeIn();
+        $('.mobile-detail-list .detail').eq(IndexNum).fadeIn();
+        $('body, html').css({'overflow':'hidden'});
+    });
+
+    $('.btn_detail-close').click(function(){
+        $('.cover').fadeOut();
+        $('.detail-box').fadeOut();
+        $('.detail').hide();
+        $('body, html').css({'overflow':'auto'});
+    });
+
+
+
+    var typed = new Typed('.typing', {
+        strings: ["a creative man.", "a man for your team.", "a wordsmith.", "pixel perfect."],
+        typeSpeed: 60,
+        loop: true
+      });
 
 
 
