7d0114ee566e59d7543b880491aae80c1439fb17
gonasooc <doupler@gmail.com>
2021-03-02 17:29:21 +0900

    v0.11 - ScrollTrigger 제외 (적용 시 디버깅 필요) - 탭메뉴 중간값 animation으로 시도해볼 것 - 각각의 작업물 코드 수정하면서 modal 안쪽 컨텐츠 채울 것

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ $(function(){
         const tabIndexNum = $(this).index()-1;
         console.log(tabIndexNum);
         $('.works-box ul').hide();
-
         $('.works-box ul').eq(tabIndexNum).show();
         $(this).addClass('on').siblings().removeClass('on');
     })
@@ -79,6 +78,8 @@ $(function(){
     //     });
     // }
 
+    // 모달에서 바깥쪽 클릭했을 때 사라지게 하는 기능 구현 필요
+
 
 
     $(window).scroll(function(){
@@ -99,9 +100,6 @@ $(function(){
         loop: true
       });
 
-    // $('#progressbar1').LineProgressbar({
-    //     percentage: 95
-    // });
 
     $('.html-bar').LineProgressbar({
         percentage: 90,
@@ -153,49 +151,51 @@ $(function(){
         $('body, html').animate({'scrollTop':contactNum});
         console.log(contactNum);
     });
-
-
     
 })
 
 
-// usage:
-batch(".works-box > ul > li > a", {
-    interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
-    batchMax: 3,   // maximum batch size (targets)
-    onEnter: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
-    onLeave: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true}),
-    onEnterBack: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
-    onLeaveBack: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true})
-    // you can also define things like start, end, etc.
-  });
+
+// // usage:
+// batch(".works-box > ul > li > a", {
+//     interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
+//     batchMax: 3,   // maximum batch size (targets)
+//     onEnter: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
+//     onLeave: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true}),
+//     onEnterBack: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
+//     onLeaveBack: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true})
+//     // you can also define things like start, end, etc.
+//   });
   
   
   
   
-  // the magical helper function (no longer necessary in GSAP 3.3.1 because it was added as ScrollTrigger.batch())...
-  function batch(targets, vars) {
-    let varsCopy = {},
-        interval = vars.interval || 0.1,
-        proxyCallback = (type, callback) => {
-          let batch = [],
-              delay = gsap.delayedCall(interval, () => {callback(batch); batch.length = 0;}).pause();
-          return self => {
-            batch.length || delay.restart(true);
-            batch.push(self.trigger);
-            vars.batchMax && vars.batchMax <= batch.length && delay.progress(1);
-          };
-        },
-        p;
-    for (p in vars) {
-      varsCopy[p] = (~p.indexOf("Enter") || ~p.indexOf("Leave")) ? proxyCallback(p, vars[p]) : vars[p];
-    }
-    gsap.utils.toArray(targets).forEach(target => {
-      let config = {};
-      for (p in varsCopy) {
-        config[p] = varsCopy[p];
-      }
-      config.trigger = target;
-      ScrollTrigger.create(config);
-    });
-  }
+//   // the magical helper function (no longer necessary in GSAP 3.3.1 because it was added as ScrollTrigger.batch())...
+//   function batch(targets, vars) {
+//     let varsCopy = {},
+//         interval = vars.interval || 0.1,
+//         proxyCallback = (type, callback) => {
+//           let batch = [],
+//               delay = gsap.delayedCall(interval, () => {callback(batch); batch.length = 0;}).pause();
+//           return self => {
+//             batch.length || delay.restart(true);
+//             batch.push(self.trigger);
+//             vars.batchMax && vars.batchMax <= batch.length && delay.progress(1);
+//           };
+//         },
+//         p;
+//     for (p in vars) {
+//       varsCopy[p] = (~p.indexOf("Enter") || ~p.indexOf("Leave")) ? proxyCallback(p, vars[p]) : vars[p];
+//     }
+//     gsap.utils.toArray(targets).forEach(target => {
+//       let config = {};
+//       for (p in varsCopy) {
+//         config[p] = varsCopy[p];
+//       }
+//       config.trigger = target;
+//       ScrollTrigger.create(config);
+//     });
+//   }
+
+
+
